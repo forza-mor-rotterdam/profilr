92fe3ea4925df33df3b5359864d8773bd4965389
Maarten de Keizer <bitbucket@maartendekeizer.nl>
2022-11-01 13:58:19 +0100

    Add a best guess implementation for login en incident controller

--- src/Controller/IncidentController.php
+++ src/Controller/IncidentController.php
@@ -3,23 +3,52 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class IncidentController extends AbstractController
 {
-    #[Route('/incident', name: 'app_incident')]
-    public function index(): Response
+    #[Route('/incident', name: 'app_incident_index')]
+    public function index(Request $request, RequestStack $requestStack, HttpClientInterface $apiClient): Response
     {
+        // if not logged in, redirect to login page
+        if ($requestStack->getSession()->get('is_logged_in') !== true) {
+            return $this->redirectToRoute('app_login_index');
+        }
+
+        // call api client
+        $incidents = $apiClient->request('GET', '/signals/v1/private/signals', [
+            'query' => [],
+            'auth_bearer' => $requestStack->getSession()->get('msb_token')
+        ])->toArray();
+
+        // render template
         return $this->render('incident/index.html.twig', [
-            'controller_name' => 'IncidentController',
+            'incidents' => $incidents
         ]);
     }
-    #[Route('/incident/{id}', name: 'app_incident')]
-    public function detail($id): Response
+
+    #[Route('/incident/{id}', name: 'app_incident_detail')]
+    public function detail(Request $request, int $id, RequestStack $requestStack, HttpClientInterface $apiClient): Response
     {
+        // if not logged in, redirect to login page
+        if ($requestStack->getSession()->get('is_logged_in') !== true) {
+            return $this->redirectToRoute('app_login_index');
+        }
+
+        // call api client
+        $incident = $apiClient->request('GET', '/signals/v1/private/signals/' . $id, [
+            'query' => [],
+            'auth_bearer' => $requestStack->getSession()->get('msb_token')
+        ])->toArray();
+
+        // render template
         return $this->render('incident/detail.html.twig', [
-            'id' => $id
+            'id' => $id,
+            'incident' => $incident
         ]);
     }
 }
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,18 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    #[Route('/login', name: 'app_login_index')]
+    public function index(Request $request, RequestStack $requestStack, HttpClientInterface $apiClient): Response
     {
+        // variabel for holding login errors
+        $error = null;
+
+        // if the submit form is submitted
+        if ($request->isMethod('POST')) {
+            // check credentials against api
+            $loginResponse = $apiClient->request('POST', '/sbmob/api/login', [
+                'body' => [
+                    'username' => $request->request->get('_username'),
+                    'password' => $request->request->get('_password'),
+                ]
+            ])->toArray();
+
+            // if success, save the received token and redirect user to incidents index page
+            if ($loginResponse['success'] === true) {
+                $requestStack->getSession()->set('msb_token', $loginResponse['result']);
+                $requestStack->getSession()->set('is_logged_in', true);
+
+                return $this->redirectToRoute('app_incident_index');
+            } else {
+                // login failed, set error message for the template
+                $error = 'invalid_username_or_password';
+            }
+        }
+
+        // render template
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'last_username' => $request->request->get('_username', ''),
+            'error' => $error
         ]);
     }
 }
