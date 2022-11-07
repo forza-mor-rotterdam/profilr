d80b3f18fd1d3701dc2aeee44aa7e26e51de2250
Remco Hoff <remco@code010.nl>
2022-11-07 14:35:18 +0100

    Load images of first 10 incidents

--- src/Controller/IncidentController.php
+++ src/Controller/IncidentController.php
@@ -23,111 +23,31 @@ class IncidentController extends AbstractController
         }
 
         // call api client
-        // $incidents = $apiClient->request('POST', 'https://diensten.rotterdam.nl/sbmob/api/msb/openmeldingen', [
-        //     'query' => [],
-        //     'body' => [
-        //         'x' => 92441,
-        //         'y' => 437718,
-        //         'radius' => 200,
-        //     ],
-        //     'auth_bearer' => $requestStack->getSession()->get('msb_token')
-        // ])->toArray()[results];
-        $incidents = json_decode('[
-              {
-                "spoed": false,
-                "datumMelding": "2021-09-10T18:58:56",
-                "datumInbehandeling": null,
-                "werkdagenSindsRegistratie": 296.0,
-                "datumRappel": null,
-                "herkomstCode": "BUR",
-                "afdeling": {
-                  "id": "204",
-                  "omschrijving": "ZZZ_SO Verkeer & Vervoer"
-                },
-                "id": 4095249,
-                "status": "Doorverwezen gekregen",
-                "onderwerp": {
-                  "id": "204",
-                  "omschrijving": "Wegdek, voetpad of rijweg"
-                },
-                "omschrijving": null,
-                "locatie": {
-                  "adres": {
-                    "straatNummer": "84948",
-                    "straatNaam": "COUWENBURG",
-                    "huisnummer": "108"
-                  },
-                  "x": 92404.05666,
-                  "y": 437814.83599
-                }
-              },
-              {
-                "spoed": false,
-                "datumMelding": "2022-10-31T13:53:19",
-                "datumInbehandeling": null,
-                "werkdagenSindsRegistratie": 6.0,
-                "datumRappel": null,
-                "herkomstCode": "BUR",
-                "afdeling": {
-                  "id": "239",
-                  "omschrijving": "Wijkregie Centrum-Delfshaven "
-                },
-                "id": 4504557,
-                "status": "Doorverwezen gekregen",
-                "onderwerp": {
-                  "id": "230",
-                  "omschrijving": "Iets anders"
-                },
-                "omschrijving": null,
-                "locatie": {
-                  "adres": {
-                    "straatNummer": "87408",
-                    "straatNaam": "DELFTSESTRAAT",
-                    "huisnummer": "5"
-                  },
-                  "x": 92363,
-                  "y": 437858
-                }
-              },
-              {
-                "spoed": false,
-                "datumMelding": "2022-11-02T09:31:50",
-                "datumInbehandeling": null,
-                "werkdagenSindsRegistratie": 4.0,
-                "datumRappel": null,
-                "herkomstCode": "BUR",
-                "afdeling": {
-                  "id": "111",
-                  "omschrijving": "Schone Stad wijkreiniging Centrum"
-                },
-                "id": 4506344,
-                "status": "Doorverwezen gekregen",
-                "onderwerp": {
-                  "id": "47",
-                  "omschrijving": "zwerfvuil op straat"
-                },
-                "omschrijving": null,
-                "locatie": {
-                  "adres": {
-                    "straatNummer": "87408",
-                    "straatNaam": "DELFTSESTRAAT",
-                    "huisnummer": "27"
-                  },
-                  "x": 92254.96999,
-                  "y": 437777.95756
-                }
-              }]');
+        $incidents = $apiClient->request('POST', 'https://diensten.rotterdam.nl/sbmob/api/msb/openmeldingen', [
+            'query' => [],
+            'body' => [
+                'x' => 92441,
+                'y' => 437718,
+                'radius' => 200,
+            ],
+            'auth_bearer' => $requestStack->getSession()->get('msb_token')
+        ])->toArray()['result'];
 
         $apiCalls = [];
-        foreach ($incidents['results'] as $k => $incident) {
+        $i = 0;
+        foreach ($incidents as $k => $incident) {
+            $i++;
+            if ($i > 10) {
+                break;
+            }
             $apiCalls[$k] = $apiClient->request('GET', 'https://diensten.rotterdam.nl/sbmob/api/msb/melding/' . $incident['id'], [
                 'query' => [],
                 'auth_bearer' => $requestStack->getSession()->get('msb_token')
             ]);
         }
-        foreach ($apiCalls as $apiCall) {
+        foreach ($apiCalls as $k => $apiCall) {
             /** @var ResponseInterface $apiCall */
-            $incidents['results']['_detail'] = $apiCall->toArray()['result'];
+            $incidents[$k]['_detail'] = $apiCall->toArray()['result'];
         }
 
         // render template
