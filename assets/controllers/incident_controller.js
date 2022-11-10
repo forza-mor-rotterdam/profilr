191fe9c35df31e6e74266bc807ca24bad62ef108
Remco Hoff <remco@code010.nl>
2022-11-10 16:07:03 +0100

    Update incident_controller.js

--- assets/controllers/incident_controller.js
+++ assets/controllers/incident_controller.js
@@ -7,22 +7,25 @@ export default class extends Controller {
     swipe(e) {
         const li = e.target.tagName.toLowerCase() !== "img" && e.target.closest("li");
         const btn = e.target.closest("button");
-        if (li && li.scrollLeft === 0) {
-            li.scrollBy({
-            left: 1,
-            behavior: "smooth"
-            });
-        } else if (!btn && li) {
-            li.scrollBy({
-            left: -1,
-            behavior: "smooth"
-            });
-        } else if (btn && li) {
-            window.location.href=`/incident/${this.idValue}/handle`;
-            li.scrollBy({
+        const anchor = e.target.closest("div.background-image")
+        if(!anchor){
+            if (li && li.scrollLeft === 0) {
+                li.scrollBy({
+                left: 1,
+                behavior: "smooth"
+                });
+            } else if (!btn && li) {
+                li.scrollBy({
                 left: -1,
                 behavior: "smooth"
-            });
+                });
+            } else if (btn && li) {
+                window.location.href=`/incident/${this.idValue}/handle`;
+                li.scrollBy({
+                    left: -1,
+                    behavior: "smooth"
+                });
+            }
         }
     }
 }
