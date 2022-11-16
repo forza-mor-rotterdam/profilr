247e76a154deb26b4785e04bcd99b97612fe2a2a
Remco Hoff <remco@code010.nl>
2022-11-16 14:47:16 +0100

    Photoviewer made interactive

--- assets/controllers/detail_controller.js
+++ assets/controllers/detail_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
         areaList: String,
         currentDistrict: String
     }
-    static targets = ['area', 'district']
+    static targets = ['area', 'district', 'selectedImage', 'thumbList']
     
     connect() {
         if(this.currentDistrictValue) {
@@ -15,6 +15,24 @@ export default class extends Controller {
         } else {
             this.areaTarget.textContent = '-'
             this.districtTarget.textContent = '-'
+        }
+        if(this.hasThumbListTarget) {
+            this.thumbListTarget.getElementsByTagName('li')[0].classList.add('selected')
+        }
+    }
+
+    selectImage(e) {
+        
+        const imgSrc = e.params.imageSource;
+        this.selectedImageTarget.src = imgSrc;
+
+        this.deselectThumbs(e.target.closest('ul'));
+        e.target.closest('li').classList.add('selected')
+    }
+
+    deselectThumbs(list) {
+        for (const item of list.querySelectorAll('li')) {
+            item.classList.remove('selected');
         }
     }
 }
