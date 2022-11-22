42181fab49cd5b6c2c2efd29b70126441398304b
Remco Hoff <remco@code010.nl>
2022-11-22 10:24:05 +0100

    Added api call for thumbnails

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -33,4 +33,24 @@ class ImageController extends AbstractController
             'Content-type' => $imageRequest->getHeaders()['content-type'][0]
         ]);
     }
+
+    #[Route('/thumbnail/{id}')]
+    public function thumbnail(Request $request, int $id, RequestStack $requestStack, HttpClientInterface $apiClient, LoggerInterface $logger): Response
+    {
+        // if not logged in, redirect to login page
+        if ($requestStack->getSession()->get('is_logged_in') !== true) {
+            $logger->warning('Not logged in', ['started' => $requestStack->getSession()->isStarted()]);
+            return $this->redirectToRoute('app_login_index');
+        }
+
+        // call api client
+        $imageRequest = $apiClient->request('GET', 'https://diensten.rotterdam.nl/sbmob/api/msb/melding/foto/' . $id, [
+            'query' => ['thumbnail' => 'true'],
+            'auth_bearer' => $requestStack->getSession()->get('msb_token')
+        ]);
+
+        return new Response($imageRequest->getContent(), Response::HTTP_OK, [
+            'Content-type' => $imageRequest->getHeaders()['content-type'][0]
+        ]);
+    }
 }
