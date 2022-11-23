0195e2085a7d6a9237691ec8525f6ca40e8fb785
Maurice Guikema <mguikema@gmail.com>
2022-11-23 11:16:36 +0100

    to_django: node changes, docker changes

--- frontend/assets/bootstrap.js
+++ frontend/assets/bootstrap.js
@@ -1,11 +1,7 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.[jt]sx?$/
-));
-
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+window.Stimulus = application
