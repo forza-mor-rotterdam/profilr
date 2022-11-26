1cea3731c1a64107a5799b70e9ea6bebd9c7879d
Maurice Guikema <mguikema@gmail.com>
2022-11-26 22:34:00 +0100

    webpack defaults

--- backend/config/webpack.py
+++ backend/config/webpack.py
@@ -7,5 +7,15 @@ class ExternalWebpackLoader(WebpackLoader):
   def load_assets(self):
     url = self.config['STATS_URL']
     response = requests.get(url)   
-    response.raise_for_status() 
-    return response.json()
+    # response.raise_for_status() 
+    if response.status_code == 200:
+      return response.json()
+    else:
+      return {
+        "status": "done",
+        "chunks": {
+          "app": [],
+          "js": {},
+        },
+        "assets": {}
+      }
