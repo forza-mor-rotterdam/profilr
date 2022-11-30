fa24c3a491d5152f47b2897c759ac3088912665e
Maurice Guikema <mguikema@gmail.com>
2022-11-30 16:16:39 +0100

    enable api backend for prod

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,7 +24,7 @@ GIT_SHA = os.environ.get("GITHUB_SHA", "no_git_sha")
 
 FRONTEND_URL = os.environ.get("FRONTEND_URL", "https://profilr.forzamor.nl")
 PROJECT_URL = os.environ.get("PROJECT_URL", FRONTEND_URL)
-ENABLE_PROFILR_API = os.environ.get("ENABLE_PROFILR_API", False) in TRUE_VALUES
+ENABLE_PROFILR_API = os.environ.get("ENABLE_PROFILR_API", True) in TRUE_VALUES
 
 INSTALLED_APPS = (
     "django.contrib.staticfiles",
