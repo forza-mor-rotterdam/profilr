b6dc116de7485d379ca607ca8d58a8e6d0aa8731
Remco Hoff <remco@code010.nl>
2022-11-30 15:16:48 +0100

    Added removing active filters

--- frontend/assets/controllers/filter/filter_controller.js
+++ frontend/assets/controllers/filter/filter_controller.js
@@ -9,17 +9,17 @@ export default class extends Controller {
     }
 
     showFilters(e) {
-        console.log('show filters')
         document.body.classList.add('show-filters')
     }
 
     hideFilters(e) {
-        console.log('hide filters')
         document.body.classList.remove('show-filters')
     }
 
     removeFilter(e) {
         e.preventDefault()
-        console.log('removeFIlter')
+        const input = document.querySelector(`[name="${e.params.description}"][value="${e.params.code}"]`);
+        input.checked = false;
+        document.getElementById('incidentFilterAllForm').submit()
     }
 }
