e74c15cbd04dba1a0a0ed1dd4014e53d70ab570c
Maurice Guikema <mguikema@gmail.com>
2022-12-12 10:50:21 +0100

    secret key env var

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,7 +4,9 @@ from os.path import join
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TRUE_VALUES = [True, "True", "true", "1"]
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", os.environ.get("APP_SECRET"))
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", os.environ.get("SECRET_KEY", os.environ.get("APP_SECRET"))
+)
 
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 DEBUG = ENVIRONMENT == "development"
