7cf7f4356765c2c445b1ead4966bcafdb79ea5a5
Maurice Guikema <mguikema@gmail.com>
2022-12-13 14:04:14 +0100

    incident handle with texts, external option

--- backend/apps/profilr/forms.py
+++ backend/apps/profilr/forms.py
@@ -2,8 +2,9 @@ from django import forms
 
 HANDLE_OPTIONS = (
     ("O", "Opgeruimd"),
-    ("N", "Kan ik niet oplossen"),
-    ("N", "Kan het niet vinden"),
+    ("N", "Niets aangetroffen"),
+    ("N", "De melding is niet voor mij"),
+    ("N", "De gemeente gaat hier niet over"),
 )
 
 
@@ -18,4 +19,14 @@ class HandleForm(forms.Form):
         label="Hoe wil je de melding afhandelen?",
         widget=RadioSelect(attrs={"class": "form-check-input"}),
         choices=[[x, HANDLE_OPTIONS[x][1]] for x in range(len(HANDLE_OPTIONS))],
+    )
+    external_text = forms.CharField(
+        label="Bericht voor de melder",
+        widget=forms.Textarea(),
+        required=True,
+    )
+    internal_text = forms.CharField(
+        label="Interne informatie",
+        widget=forms.Textarea(),
+        required=False,
     )
