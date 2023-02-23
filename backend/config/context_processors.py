5a04d54f0f36b57c10c7ff76768332f610b71bc3
Remco Hoff <remco@code010.nl>
2023-02-23 15:53:24 +0100

    Use saved fontsize onload, hide menu on loginpage

--- backend/config/context_processors.py
+++ backend/config/context_processors.py
@@ -2,11 +2,14 @@ import requests
 from django.conf import settings
 
 
-def general_settings(context):
+def general_settings(request):
     PROFILE_API_AVAILABLE = False
     profile_api_response = requests.get(settings.PROFILE_API_HEALTH_CHECK_URL)
     if profile_api_response and profile_api_response.status_code == 200:
         PROFILE_API_AVAILABLE = True
+
+    # request.user.profile
+    profile = request.user.profile if hasattr(request, "user") else None
 
     return {
         "git_sha": settings.GIT_SHA,
@@ -18,4 +21,5 @@ def general_settings(context):
         "FRONTEND_URL": settings.FRONTEND_URL,
         "PROJECT_URL": settings.PROJECT_URL,
         "UI_SETTINGS": settings.UI_SETTINGS,
+        "profile": profile,
     }
